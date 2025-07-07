2565a696a2aa4fd0cd9cbcefbe37a4facb572da9
Amit Gupta <amitguptaearn@gmail.com>
2025-07-07 18:14:31 +0530

    Add name initials

--- lib/full_name_gem.rb
+++ lib/full_name_gem.rb
@@ -7,8 +7,12 @@ require "full_name_gem/railtie" if defined?(Rails)
 module FullNameGem
   class Error < StandardError; end
   module FullName
-    def f_name
+    def full_name
       "#{first_name} #{last_name}"
+    end
+    
+    def name_initials
+      [first_name, last_name].map { |n| "#{n[0]}." }.join(' ')
     end
   end
 end
