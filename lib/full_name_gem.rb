ad3a3b47b426995305f6b917400f77e105ae8dd7
Amit Gupta <amitguptaearn@gmail.com>
2025-07-07 17:14:34 +0530

    Add gem logic

--- lib/full_name_gem.rb
+++ lib/full_name_gem.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "full_name_gem/version"
+require "full_name_gem/version"
+require "full_name_gem/railtie" if defined?(Rails)
 
 module FullNameGem
   class Error < StandardError; end
-  # Your code goes here...
+  def f_name
+    "#{first_name} #{last_name}"
+  end
 end
